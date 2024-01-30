2be18e04d715aa5d8efd222ffa1ce1db47ef7bf1
yudistjr <wahyudistiragunners@gmail.com>
2024-01-30 09:47:53 +0700

    Fix nama petugas on print

--- application/views/cetak.php
+++ application/views/cetak.php
@@ -14,40 +14,42 @@ $no = 1;
 $query = mysqli_query($mysqli, "SELECT a.id_barang,a.nama_barang,a.id_jenis,a.id_satuan,a.stok,b.id_jenis,b.nama_jenis,c.id_satuan,c.nama_satuan 
                                 FROM is_barang as a INNER JOIN is_jenis_barang as b INNER JOIN is_satuan as c
                                 ON a.id_jenis=b.id_jenis AND a.id_satuan=c.id_satuan ORDER BY id_barang DESC")
-                                or die('Ada kesalahan pada query tampil Data Barang: '.mysqli_error($mysqli));
+    or die('Ada kesalahan pada query tampil Data Barang: ' . mysqli_error($mysqli));
 $count  = mysqli_num_rows($query);
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml"> <!-- Bagian halaman HTML yang akan konvert -->
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-        <title>Laporan Stok Barang</title>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/laporan.css" />
-    </head>
-    <body>
-        <div id="title">
-            LAPORAN STOK BARANG GUDANG MATERIAL 
-        </div>
-        
-        <hr><br>
 
-        <div id="isi">
-            <table width="100%" border="0.3" cellpadding="0" cellspacing="0">
-                <thead style="background:#e8ecee">
-                    <tr class="tr-title">
-                        <th height="20" align="center" valign="middle">No.</th>
-                        <th height="20" align="center" valign="middle">ID Barang</th>
-                        <th height="20" align="center" valign="middle">Nama Barang</th>
-                        <th height="20" align="center" valign="middle">Jenis Barang</th>
-                        <th height="20" align="center" valign="middle">Stok</th>
-                        <th height="20" align="center" valign="middle">Satuan</th>
-                    </tr>
-                </thead>
-                <tbody>
-        <?php
-        // tampilkan data
-        while ($data = mysqli_fetch_assoc($query)) {
-            // menampilkan isi tabel dari database ke tabel di aplikasi
-            echo "  <tr>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>Laporan Stok Barang</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/laporan.css" />
+</head>
+
+<body>
+    <div id="title">
+        LAPORAN STOK BARANG GUDANG MATERIAL
+    </div>
+
+    <hr><br>
+
+    <div id="isi">
+        <table width="100%" border="0.3" cellpadding="0" cellspacing="0">
+            <thead style="background:#e8ecee">
+                <tr class="tr-title">
+                    <th height="20" align="center" valign="middle">No.</th>
+                    <th height="20" align="center" valign="middle">ID Barang</th>
+                    <th height="20" align="center" valign="middle">Nama Barang</th>
+                    <th height="20" align="center" valign="middle">Jenis Barang</th>
+                    <th height="20" align="center" valign="middle">Stok</th>
+                    <th height="20" align="center" valign="middle">Satuan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // tampilkan data
+                while ($data = mysqli_fetch_assoc($query)) {
+                    // menampilkan isi tabel dari database ke tabel di aplikasi
+                    echo "  <tr>
                         <td width='40' height='13' align='center' valign='middle'>$no</td>
                         <td width='80' height='13' align='center' valign='middle'>$data[id_barang]</td>
                         <td style='padding-left:5px;' width='215' height='13' valign='middle'>$data[nama_barang]</td>
@@ -55,24 +57,25 @@ $count  = mysqli_num_rows($query);
                         <td style='padding-right:10px;' width='80' height='13' align='right' valign='middle'>$data[stok]</td>
                         <td style='padding-left:5px;' width='80' height='13' valign='middle'>$data[nama_satuan]</td>
                     </tr>";
-            $no++;
-        }
-        ?>  
-                </tbody>
-            </table>
+                    $no++;
+                }
+                ?>
+            </tbody>
+        </table>
 
-            <div id="footer-tanggal">
-                Bandarlampung, <?php echo tgl_eng_to_ind("$hari_ini"); ?>
-            </div>
-            <div id="footer-jabatan">
-                Pimpinan
-            </div>
-            
-            <div id="footer-nama">
-                Indra Setyawantoro, S.Kom.
-            </div>
+        <div id="footer-tanggal">
+            Bandarlampung, <?php echo tgl_eng_to_ind("$hari_ini"); ?>
         </div>
-    </body>
+        <div id="footer-jabatan">
+            Pimpinan
+        </div>
+
+        <div id="footer-nama">
+            <?= $_SESSION['nama_user'] ?>
+        </div>
+    </div>
+</body>
+
 </html><!-- Akhir halaman HTML yang akan di konvert -->
 <?php
 // $filename="LAPORAN STOK BARANG GUDANG MATERIAL.pdf"; //ubah untuk menentukan nama file pdf yang dihasilkan nantinya
